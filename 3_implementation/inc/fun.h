2280809a0478f15d1c934b9c93ec4ff57a38fc44
Sabitha99 <94462726+Sabitha99@users.noreply.github.com>
2021-11-22 18:36:42 +0530

    Create fun.h

---
+++ 3_implementation/inc/fun.h
@@ -0,0 +1,10 @@
+
+#ifndef _FUN_H
+#define _FUN_H
+void addfingerprint();
+void deletefingerprint();
+void searchfingerprint();
+void listfingerprint();
+void unlock();
+void modifyfingerprint();
+#endif
